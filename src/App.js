f31a6dd8eae3b3d543aa4a65c0b8990970a9a099
sachinpunera <sachin.punera1@gmail.com>
2023-01-29 17:08:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
-
+import {Home} from './components/Home'
+import { VideoCall } from "./components/VideoCall/VideoCall";
+// import bg_img from "../asset/img/bg_img.png"
 const socket = io.connect("http://localhost:3001");
 
 function App() {
@@ -18,9 +22,25 @@ function App() {
   }, [socket]);
 
   return (
-    <>
-      <div className="App">
-        <input
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/videoCall" element={<VideoCall/>}></Route>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
+
+//   const myStyle={
+//     backgroundImage: 
+//     `url(${bg_img})`,
+   
+//     backgroundSize: 'cover',
+//     backgroundRepeat: 'no-repeat',
+// };
+{/* <input
           type="text"
           placeholder="message"
           onChange={(e) => {
@@ -29,10 +49,4 @@ function App() {
         />
         <button onClick={sendMessage}>Send Message</button>
         <h1>Message:</h1>
-          {messageReceived}
-      </div>
-    </>
-  );
-}
-
-export default App;
+          {messageReceived} */}
